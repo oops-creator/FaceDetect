51c1a1374592cb64a648a5694788a0f592fdedea
oops-creator <still.shivam@gmail.com>
2020-04-13 04:35:31 +0530

    Add second pane to show 'currently comparing to' image

--- GUI/app.py
+++ GUI/app.py
@@ -1,16 +1,47 @@
 from tkinter import *
 from PIL import ImageTk 
 from PIL import Image as Img
-
+import time
+import os
+import threading
 
 
 root = Tk()
 
 
-originalImage = ImageTk.PhotoImage(Img.open('/home/escanor/Documents/DSP/Facial-Recognition/database/1.jpeg'))
+#Show the original image on left hand side  
+originalImage = ImageTk.PhotoImage(
+                Img.open('/home/escanor/Documents/DSP/Facial-Recognition/database/1.jpeg'))
+originalSection = Label(root , image=originalImage ,  width=600 , height=600)
+originalSection.grid(row=0,column=0)
 
-originalSection = Label(root , image=originalImage)
 
-originalSection.pack()
+#Get all the images from 'database'
+
+currentDir = os.getcwd()
+images = os.listdir(os.path.join(currentDir,'database'))
+dbImages = [os.path.join(currentDir ,'database', image) for image in images]
+
+
+
+#Show the comparable image on right hand side
+# comparingImage = ImageTk.PhotoImage(
+#                 Img.open('/home/escanor/Documents/DSP/Facial-Recognition/database/1.jpeg'))
+comparingSection = Label(root, width=600 , height=600)
+comparingSection.grid(row=0,column=1)
+
+
+def changeImage():
+    for i in dbImages:
+        time.sleep(1)
+        changedImage = ImageTk.PhotoImage(
+                    Img.open(i))
+        print(i)
+        comparingSection.configure(image=changedImage)
+   
+
+thread = threading.Thread(target=changeImage)
+thread.start()
+
 
 root.mainloop()
